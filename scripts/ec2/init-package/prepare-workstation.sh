02f0aae0c0c4f8d9476f3b39d737cf3f64537784
Suman Mukherjee <sumanmukherjee03@gmail.com>
2018-05-20 16:03:17 -0700

    Update the prepare-workstation script

--- scripts/ec2/init-package/prepare-workstation.sh
+++ scripts/ec2/init-package/prepare-workstation.sh
@@ -16,36 +16,37 @@ err() {
 ################### Helper functions ####################
 #########################################################
 setup_pyenv() {
-  [[ -d ~/.pyenv ]] && rm -rf ~/.pyenv
-  git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-  echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-  echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-  echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-  pip install virtualenv virtualenvwrapper
-  git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-  echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-  . ~/.bashrc
+  if [[ ! -d $HOME/.pyenv && cat $HOME/.bashrc | grep -i 'pyenv' ]]; then
+    git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $HOME/.bashrc
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $HOME/.bashrc
+    echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
+    pip install virtualenv virtualenvwrapper
+    git clone https://github.com/pyenv/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
+    echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
+  fi
+  . $HOME/.bashrc
 }
 
 setup_ssh_keys_and_tokens() {
-  if [[ ! -f ~/.ssh/id_rsa && ! -f ~/.ssh/id_rsa.pub ]]; then
-    ssh-keygen -t rsa -N "" -b 4096 -C "ssh private key" -f ~/.ssh/id_rsa
-    chmod 600 ~/.ssh/id_rsa
-    chmod 600 ~/.ssh/id_rsa.pub
+  if [[ ! -f $HOME/.ssh/id_rsa && ! -f $HOME/.ssh/id_rsa.pub && ! -f $HOME/.ssh/authorized_keys ]]; then
+    ssh-keygen -t rsa -N "" -b 4096 -C "ssh private key" -f $HOME/.ssh/id_rsa
+    chmod 600 $HOME/.ssh/id_rsa
+    chmod 600 $HOME/.ssh/id_rsa.pub
+    touch $HOME/.ssh/authorized_keys
+    cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+    chmod 600 $HOME/.ssh/authorized_keys
   fi
-  touch ~/.ssh/authorized_keys
-  cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-  chmod 600 ~/.ssh/authorized_keys
-  ssh-keyscan -H localhost >> ~/.ssh/known_hosts
+  ssh-keyscan -H localhost >> $HOME/.ssh/known_hosts
   echo "Updated ssh"
 }
 
 setup_workstation() {
   pushd .
-  . ~/.bashrc
-  cd ~
-  [[ -d ~/workstation ]] && rm -rf ~/workstation
-  git clone https://github.com/sumanmukherjee03/workstation.git ~/workstation
+  . $HOME/.bashrc
+  cd $HOME
+  [[ -d $HOME/workstation ]] && rm -rf $HOME/workstation
+  git clone https://github.com/sumanmukherjee03/workstation.git $HOME/workstation
   cd workstation
   . .env
   make build HOST_IP=localhost DRY_RUN=off
